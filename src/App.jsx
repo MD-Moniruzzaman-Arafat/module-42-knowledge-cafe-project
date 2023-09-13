db8b80d9fa5eeb19e05e71a2d2c77369b3eacfc4
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-09-13 21:01:50 +0600

    header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-center text-6xl'>Knowledge cafe</h1>
+      <Header></Header>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <>
+            <div className="flex justify-between items-center mx-20 border-b-2 pb-2">
+                <h1 className='text-center text-4xl'>Knowledge Cafe</h1>
+                <img src="../../../public/images/profile.png" alt="" />
+            </div>
+        </>
+    );
+};
+
+export default Header;
